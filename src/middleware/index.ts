7de291d4f1c67a2e007c510894c94bff04c693a0
raphdcst <rdecostaire@gmail.com>
2026-06-07 20:30:06 +0200

    feat(ui): add in build page and middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,26 @@
+import { defineMiddleware } from 'astro:middleware';
+import { IN_BUILD, ALLOW_POLICIES_IN_BUILD } from 'astro:env/client';
+
+export const onRequest = defineMiddleware((context, next) => {
+	const url = new URL(context.request.url);
+
+	if (
+		url.pathname === '/in-build' ||
+		url.pathname.includes('_emdash/admin') ||
+		url.pathname.includes('_emdash/api')
+	) {
+		return next();
+	}
+
+	const isPolicyRequest = url.pathname.includes('policies');
+
+	if (!IN_BUILD) {
+		return next();
+	}
+
+	if (isPolicyRequest && ALLOW_POLICIES_IN_BUILD) {
+		return next();
+	}
+
+	return context.redirect('/in-build', 307);
+});
