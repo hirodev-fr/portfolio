c2f24ac3f03a7c373fefe1d4eff5fb4a87650dfd
raphdcst <rdecostaire@gmail.com>
2026-08-04 15:58:01 +0200

    fix(env): load env vars with vite dedicated api instead of node process

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,18 @@ import { defineConfig, fontProviders } from 'astro/config';
 import emdash, { s3 } from 'emdash/astro';
 import { libsql } from 'emdash/db';
 
+import { loadEnv } from 'vite';
+
+const {
+	LIBSQL_DATABASE_URL,
+	LIBSQL_AUTH_TOKEN,
+	S3_PUBLIC_URL,
+	S3_ENDPOINT,
+	S3_BUCKET,
+	S3_ACCESS_KEY_ID,
+	S3_SECRET_ACCESS_KEY,
+} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
 export default defineConfig({
 	output: 'server',
 	adapter: node({ mode: 'standalone' }),
@@ -18,16 +30,15 @@ export default defineConfig({
 		react(),
 		emdash({
 			database: libsql({
-				url: process.env.LIBSQL_DATABASE_URL,
-				authToken: process.env.LIBSQL_AUTH_TOKEN,
+				url: LIBSQL_DATABASE_URL,
+				authToken: LIBSQL_AUTH_TOKEN,
 			}),
-
 			storage: s3({
-				endpoint: process.env.S3_ENDPOINT,
-				bucket: process.env.S3_BUCKET,
-				accessKeyId: process.env.S3_ACCESS_KEY_ID,
-				secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-				publicUrl: process.env.S3_PUBLIC_URL,
+				endpoint: S3_ENDPOINT,
+				bucket: S3_BUCKET,
+				accessKeyId: S3_ACCESS_KEY_ID,
+				secretAccessKey: S3_SECRET_ACCESS_KEY,
+				publicUrl: S3_PUBLIC_URL,
 			}),
 		}),
 	],
