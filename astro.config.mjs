967f60702711b70b072509d0ce579ff53706cfd9
raphdcst <rdecostaire@gmail.com>
2026-08-08 19:23:49 +0200

    fix(config): use process to load env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 import emdash, { s3 } from 'emdash/astro';
 import { postgres } from 'emdash/db';
 
-import { loadEnv } from 'vite';
-
-const {
-	POSTGRES_URL,
-	S3_PUBLIC_URL,
-	S3_ENDPOINT,
-	S3_BUCKET,
-	S3_ACCESS_KEY_ID,
-	S3_SECRET_ACCESS_KEY,
-} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
-
 export default defineConfig({
 	output: 'server',
 	adapter: node({ mode: 'standalone' }),
@@ -29,14 +18,14 @@ export default defineConfig({
 		react(),
 		emdash({
 			database: postgres({
-				connectionString: POSTGRES_URL,
+				connectionString: process.env.POSTGRES_URL,
 			}),
 			storage: s3({
-				endpoint: S3_ENDPOINT,
-				bucket: S3_BUCKET,
-				accessKeyId: S3_ACCESS_KEY_ID,
-				secretAccessKey: S3_SECRET_ACCESS_KEY,
-				publicUrl: S3_PUBLIC_URL,
+				endpoint: process.env.S3_ENDPOINT,
+				bucket: process.env.S3_BUCKET,
+				accessKeyId: process.env.S3_ACCESS_KEY_ID,
+				secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+				publicUrl: process.env.S3_PUBLIC_URL,
 			}),
 		}),
 	],
